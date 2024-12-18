92813af879e50a799580f8ee177a5bb562301c4d
carlos <21239682+candradec@users.noreply.github.com>
2024-12-18 16:36:53 +0000

    linting

--- noxfile.py
+++ noxfile.py
@@ -12,10 +12,6 @@ def isort(session):
 def flake8(session):
     session.run("flake8", "pypsa_eur")
 
-# @nox.session(tags=["style", "fix"])
-# def mypy(session):
-#     session.run("mypy", "pypsa_eur", "--check-untyped-defs")
-
 @nox.session(tags=["style", "fix"])
 def autopep8(session):
     session.run("autopep8", "pypsa_eur", "--in-place", "-r")
